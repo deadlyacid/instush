8fb464b8ec51c287ff79c29c6f609d096c3c825e
unknown <eladb@Elad-Pc.idmoo.local>
2015-04-13 14:26:44 +0300

    instush-commit

---
+++ app/src/main/java/com/example/eladb/myapplication/ApplicationData.java
@@ -0,0 +1,10 @@
+package com.example.eladb.myapplication;
+
+/**
+ * Created by eladb on 05/04/2015.
+ */
+public class ApplicationData {
+    public static final String CLIENT_ID = "5500f8da087e47fd8a717c79c325883a";
+    public static final String CLIENT_SECRET = "83cf3a8a93804754b1677d33ee9d05ff";
+    public static final String CALLBACK_URL = "http://play.idomoo.com";
+}
